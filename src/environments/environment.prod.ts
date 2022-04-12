9efffdf88e0c9a36133814a4d93a349a489d0236
Drishya Dinesh <drishyadinesh83@gmail.com>
2022-04-12 07:26:44 -0400

    enviornment updates

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   firebase: {
-    projectId: 'test-21c6c',
-    appId: '1:265274952614:web:3f2be7b112c8e5809d5d67',
-    storageBucket: 'test-21c6c.appspot.com',
     locationId: 'us-central',
-    apiKey: 'AIzaSyDisC7pdSA7cx2EkzriggJe1ykfq6hgDO4',
-    authDomain: 'test-21c6c.firebaseapp.com',
-    messagingSenderId: '265274952614',
+    apiKey: 'AIzaSyA6Qmic4E8UuDB8c8XWlK3ZU-XyQUWdt7U',
+    authDomain: 'test1-f647c.firebaseapp.com',
+    projectId: 'test1-f647c',
+    storageBucket: 'test1-f647c.appspot.com',
+    messagingSenderId: '256004700329',
+    appId: '1:256004700329:web:bd3311a920afe03ab411b8',
   },
   production: true,
 };
